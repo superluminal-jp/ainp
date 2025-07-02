28c08a2eff0f5c042f987618d586aac2f0f58303
Taiki Ogihara <taiki.ogihara@gmail.com>
2025-07-03 07:12:25 +0900

    remove tools

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,7 +4,6 @@ import { data } from "./data/resource";
 import { storage } from "./storage/resource";
 import { chatBedrockFunction } from "./functions/chat-bedrock/resource";
 import { embedFilesFunction } from "./functions/embed-files/resource";
-import { executeToolFunction } from "./functions/execute-tool/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -15,7 +14,6 @@ const backend = defineBackend({
   storage,
   chatBedrockFunction,
   embedFilesFunction,
-  executeToolFunction,
 });
 
 // Grant the embed-files function access to the storage bucket
@@ -26,11 +24,6 @@ backend.storage.resources.bucket.grantReadWrite(
 // Grant the chat-bedrock function access to the storage bucket (read-only for FAISS indexes)
 backend.storage.resources.bucket.grantRead(
   backend.chatBedrockFunction.resources.lambda
-);
-
-// Grant the execute-tool function access to the storage bucket (read-only for tool code)
-backend.storage.resources.bucket.grantRead(
-  backend.executeToolFunction.resources.lambda
 );
 
 // Add bucket name to embed-files function environment
@@ -45,19 +38,7 @@ backend.chatBedrockFunction.addEnvironment(
   backend.storage.resources.bucket.bucketName
 );
 
-// Add execute-tool function name to chat-bedrock function environment
-backend.chatBedrockFunction.addEnvironment(
-  "EXECUTE_TOOL_FUNCTION_NAME",
-  backend.executeToolFunction.resources.lambda.functionName
-);
-
-// Add bucket name to execute-tool function environment
-backend.executeToolFunction.addEnvironment(
-  "STORAGE_BUCKET_NAME",
-  backend.storage.resources.bucket.bucketName
-);
-
 // Grant chat-bedrock function permission to invoke execute-tool function
-backend.executeToolFunction.resources.lambda.grantInvoke(
+backend.chatBedrockFunction.resources.lambda.grantInvoke(
   backend.chatBedrockFunction.resources.lambda
 );
