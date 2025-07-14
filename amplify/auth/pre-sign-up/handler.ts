a2fe7e3986370a7b3288d29b1d5e9987e80dd7cc
Taiki Ogihara <taiki.ogihara@gmail.com>
2025-07-14 21:53:43 +0900

    email filtering and account settings

--- amplify/auth/pre-sign-up/handler.ts
+++ amplify/auth/pre-sign-up/handler.ts
@@ -1,10 +1,9 @@
 import type { PreSignUpTriggerHandler } from "aws-lambda";
-import { env } from "$amplify/env/pre-sign-up";
 
 export const handler: PreSignUpTriggerHandler = async (event) => {
   const email = event.request.userAttributes["email"];
 
-  if (!email.endsWith(env.ALLOW_DOMAINS)) {
+  if (!email.endsWith(process.env.ALLOW_DOMAINS!)) {
     throw new Error("Invalid email domain");
   }
 
